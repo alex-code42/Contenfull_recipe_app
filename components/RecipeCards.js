ad41314c062b40a420c5103c524cb38a63a31023
Alex <areichel1986@gmail.com>
2023-11-24 12:56:05 +0100

    yeah

--- components/RecipeCards.js
+++ components/RecipeCards.js
@@ -1,12 +1,18 @@
 import Image from "next/image"
 
+
+
 export default function RecipeCard({recipe}){
-    console.log('https:' + recipe.fields.featuredImage.fields.file.url)
+
+  const {titlebig, ingredients} = recipe.fields
+  const {id} = recipe.sys
+  const featuredImageUrl = recipe.fields.featuredImage.fields.file.url
+  
     return(
-    <div key={recipe.sys.id}>
-        <Image src={'https:' + recipe.fields.featuredImage.fields.file.url} width={500} height={500} />
-          <h2>{ recipe.fields.titlebig}</h2>
-          <p>{ recipe.fields.ingredients}</p>
+    <div key={id}>
+        <Image src={'https:' + featuredImageUrl} width={500} height={500} />
+          <h2>{ titlebig}</h2>
+          <p>{ ingredients}</p>
         </div>
     )
 }
